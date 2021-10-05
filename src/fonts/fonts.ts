1ef2962bd97862d6c0817ef79c22270fcd6720ae
pact0 <kuba.kowalski21331@gmail.com>
2021-10-05 09:48:44 +0200

    fixed font

--- src/fonts/fonts.ts
+++ src/fonts/fonts.ts
@@ -1,9 +1,12 @@
 import { createGlobalStyle } from "styled-components";
-import FontinSmallCaps from "./Fontin-Regular.ttf";
+import FontinSmallCaps from "./Fontin-SmallCaps.ttf";
 
 export default createGlobalStyle`
     @font-face {
         font-family: 'Fontin SmallCaps';
-        url(${FontinSmallCaps}) format('truetype'),
+        src: local('Fontin SmallCaps'), local('FontinSmallCaps'),
+        url(${FontinSmallCaps}) format('truetype');
+        font-weight: 300;
+        font-style: normal;
     }
 `;
